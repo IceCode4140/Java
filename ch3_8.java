1a0d45be55c4972ca043553da571a96bb0f1208b
H. Abe <73382016+IceCode4140@users.noreply.github.com>
2021-02-16 22:33:43 +0900

    practice ch3_8

---
+++ ch3_8.java
@@ -0,0 +1,16 @@
+
+public class ch3_8 {
+ public static void main(String[] args) {
+  int x;
+  long y;
+  x = 103;
+  System.out.printf(" print value of 103 = " + x);// decimal
+  x = 0b111;
+  System.out.printf(" print value of 0b111 = " + x);// binary
+  y = 022;
+  System.out.printf(" print value of 022 = " + y);// octal
+  y = 0x2B;
+  System.out.printf(" print value of 0x2B = " + y);// hexadecimal
+ }
+
+}
