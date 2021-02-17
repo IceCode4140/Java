0f41b471fad01180e58eaaf437a41da35ebece44
H. A <73382016+IceCode4140@users.noreply.github.com>
2021-02-17 23:51:25 +0900

    practice ch3_11

---
+++ ch3_11.java
@@ -0,0 +1,10 @@
+
+public class ch3_11 {
+ public static void main(String[] args) {
+  System.out.printf(" byte range %d ~ %d%n ", Byte.MIN_VALUE, Byte.MAX_VALUE);
+  System.out.printf(" short range %d ~ %d%n ", Short.MIN_VALUE, Short.MAX_VALUE);
+  System.out.printf(" int range %d ~ %d%n ", Integer.MIN_VALUE, Integer.MAX_VALUE);
+  System.out.printf(" long range %d ~ %d%n ", Long.MIN_VALUE, Long.MAX_VALUE);
+ }
+
+}
