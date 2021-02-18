5c013def535deb7dd0436088da09fdd18c68f4c3
H. A <73382016+IceCode4140@users.noreply.github.com>
2021-02-18 23:50:32 +0900

    practice ch4_5

---
+++ ch4_5.java
@@ -0,0 +1,12 @@
+
+public class ch4_5 {
+ public static void main(String[] args) {
+  int students = 20;
+  int grapes = 90;
+  int count = grapes / students;
+  int left = grapes % students;
+  System.out.println(" how many grapes for per students = " + count);
+  System.out.println(" left how many grapes = " + left);
+ }
+
+}
