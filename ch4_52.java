ea3f82a62b57dffed12c79b4290536e1c2df5534
H. A <73382016+IceCode4140@users.noreply.github.com>
2021-07-10 18:24:08 +0900

    practice ch4_52

---
+++ ch4_52.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ch4_52 {
+  public static void main(String[] args) {
+  int start = 1;
+  int end = 100;
+  int sum = 0 ;
+
+  sum = (start + end ) * end/2;
+
+  System.out.println(" add 1 to 100 = " + sum);
+
+   }
+}
